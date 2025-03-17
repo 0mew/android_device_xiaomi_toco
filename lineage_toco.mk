983cfde35dbe05977f761d486bfaf5f87333d6a7
DanielBorkun <fwl.hh@icloud.com>
2025-03-17 19:40:34 +0000

    toco: risingOS BringUp

--- lineage_toco.mk
+++ lineage_toco.mk
@@ -15,6 +15,17 @@ $(call inherit-product, device/xiaomi/toco/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+## risingOS-Flags
+
+# Addons
+TARGET_HAS_UDFPS := true
+
+# Gms 
+WITH_GMS := true
+
+# rising Maintainer
+RISING_MAINTAINER=fwlhh
+
 PRODUCT_NAME := lineage_toco
 PRODUCT_DEVICE := toco
 PRODUCT_BRAND := Xiaomi
@@ -25,4 +36,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="toco-user 12 RKQ1.210614.002 V13.0.6.0.SFNEUXM release-keys" \
-    BuildFingerprint=Xiaomi/toco_eea/toco:12/RKQ1.210614.002/V13.0.6.0.SFNEUXM:user/release-keys
+    BuildFingerprint=Xiaomi/toco_eea/toco:12/RKQ1.210614.002/V13.0.6.0.SFNEUXM:user/release-keys \
+    RisingMaintainer="fwlhh" \
+    RisingChipset="SD 730G"
