320b104c713a131989ce35847e7ec8f6f45433b2
0mew <omerfarukyerlikaya28@gmail.com>
2025-12-05 12:24:06 +0300

    fix

--- device.mk
+++ device.mk
@@ -13,11 +13,9 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 # MiuiCamera
 $(call inherit-product-if-exists, device/xiaomi/miuicamera-toco/device.mk)
 
-# LED İzinleri (Init Script)
-PRODUCT_COPY_FILES += \
-    device/xiaomi/toco/init.toco_leds.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.toco_leds.rc
 
-    
+PRODUCT_COPY_FILES += \
+    device/xiaomi/toco/rootdir/etc/init.toco_leds.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.toco_leds.rc
 
 # Audio
 PRODUCT_COPY_FILES += \
